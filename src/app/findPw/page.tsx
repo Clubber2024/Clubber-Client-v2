5bef374435511ceb6662be2de6e3833f4bd0ce12
Kangyeeun0 <kangyeeun55@gmail.com>
2025-08-07 22:51:44 +0900

    fix: 파일구조 변경

--- src/app/findPw/page.tsx
+++ src/app/findPw/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import FindPw from '@/components/features/login/findpw/api/findPw';
+import FindPw from '@/components/features/login/findpw/FindPw';
 
 export default function FindPwPage() {
   return <FindPw />;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Login from '@/components/features/login/api/login';
+import Login from '@/components/features/login/Login';
 
 export default function LoginPage() {
   return (
