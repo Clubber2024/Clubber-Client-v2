977f95a2058d15bcd9ab5e12be1ccf610bbaf171
Kangyeeun0 <kangyeeun55@gmail.com>
2025-08-07 16:14:26 +0900

    ff

--- src/app/admin/recruitContent/page.tsx
+++ src/app/admin/recruitContent/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import RecruitContent from '@/components/features/admin/recruit/RecruitContent';
+import dynamic from 'next/dynamic';
+
+// import RecruitContent from '@/components/features/admin/recruit/RecruitContent';
+// import dynamic from 'next/dynamic';
+
+const RecruitContent = dynamic(() => import('@/components/features/admin/recruit/RecruitContent'), {
+  ssr: false,
+});
 
 export default function RecruitContentPage() {
   return <RecruitContent />;
