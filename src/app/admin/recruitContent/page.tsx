a6f451b2db7b360b30b71893527af223c5855ecb
Kangyeeun0 <kangyeeun55@gmail.com>
2025-08-07 16:07:44 +0900

    ff

--- src/app/admin/recruitContent/page.tsx
+++ src/app/admin/recruitContent/page.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import RecruitContent from '@/components/features/admin/recruit/RecruitContent';
-import { Suspense } from 'react';
 
 export default function RecruitContentPage() {
-  return (
-    <>
-      <RecruitContent />
-    </>
-  );
+  return <RecruitContent />;
 }
