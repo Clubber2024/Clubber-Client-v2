1f942e22d855b8815bd636f758a8b5fe8f97e127
Kangyeeun0 <kangyeeun55@gmail.com>
2025-08-07 15:58:49 +0900

    fix

--- src/app/admin/recruitContent/page.tsx
+++ src/app/admin/recruitContent/page.tsx
@@ -1,12 +1,12 @@
+'use client';
+
 import RecruitContent from '@/components/features/admin/recruit/RecruitContent';
 import { Suspense } from 'react';
 
 export default function RecruitContentPage() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <RecruitContent />
-      </Suspense>
+      <RecruitContent />
     </>
   );
 }
